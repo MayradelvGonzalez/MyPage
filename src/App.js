7266facdd0b6e14650458a622e899e30352c500f
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-02-21 12:25:47 -0300

    starting

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import pc from './images/pc.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pc} className="App-logo" alt="logo" />
+            <p className='title'>
+         Welcome!
+        </p>
+        
+        <div className="Links">
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My linkedin 🧷
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/MayradelvGonzalez"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My github 🧷
+        </a></div>
+      </header>
+    </div>
+
+);
+}
+
+export default App;
